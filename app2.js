39f8fda26d3ee0f5d8a5501c6447511cb067038d
Kirill Shishov <skireal@mail.ru>
2021-12-15 18:04:41 +0300

    optimize, add comments

--- app2.js
+++ app2.js
@@ -10,7 +10,7 @@ const allMarketData = 'https://mfd.ru/marketdata/?id=5&mode=3&group=16';
 const blueChipsMarketData = 'https://mfd.ru/marketdata/?id=5&mode=1';
 const minVolume = 2000000;
 const hourFactor = new Date().getHours() / 10;
-const condition = 0.1;
+const condition = 1;
 
 bot.startPolling();
 
@@ -25,7 +25,17 @@ function getDate() {
 async function getData() {
   let browser = await Puppeteer.launch();
   let page = await browser.newPage();
-  await page.goto(allMarketData);
+  await page.setRequestInterception(true);
+  page.on('request', (request) => {
+    if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
+
+  //Делаем запрос к нужной нам странице (дожидаемся только построения DOM)
+  await page.goto(allMarketData, { waitUntil: 'domcontentloaded' });
   await page.click('.m-button-link-dotted');
   await page.type('#mfd-logon-dialog input[name=username]', 'skireal@mail.ru');
   await page.type('#mfd-logon-dialog input[name=password]', 'puka666');
@@ -39,10 +49,14 @@ setInterval(() => {
     try {
       let page = await getData();
 
+      //Парсим DOM
+
       let result = await page.evaluate(
         (minVolume, hourFactor) => {
           let firstValues = [];
           let indexes = [];
+
+          //Парсим первое значение процентов и объемы сделок. Добавляем в массив подходящие акции. Добавляем номера этих элементов в отдельный массив indexes
           let $dayVolumes = document.querySelectorAll('.mfd-table tr td:nth-child(12)');
           let $firstValues = document.querySelectorAll('.mfd-table tr td:nth-child(5)');
 
@@ -58,30 +72,24 @@ setInterval(() => {
             }
           });
 
-          //Парсим названия акций
-          let tickerNames = [];
-          let $tickerNames = document.querySelectorAll('.mfd-table tr td:nth-child(1)');
-          $tickerNames.forEach((tickerName, index) => {
-            if (indexes.includes(index)) {
-              tickerNames.push(tickerName.textContent);
-            }
-          });
-
           return { firstValues, indexes };
         },
         minVolume,
         hourFactor
       );
 
-      //     console.log(result['firstValues'], getDate(), 'первый');
+      console.log(result['firstValues'], getDate(), 'первый');
 
-      let indexes = [];
-      indexes = result['indexes'];
-      console.log(indexes, 'aaaa');
-      process.on('warning', e => console.warn(e.stack))
+      process.on('warning', (e) => console.warn(e.stack));
+
+      //Ждем X min и перезагружаем страницу
       await new Promise((resolve, reject) => setTimeout(resolve, 10 * min));
       await page.reload();
 
+      let indexes = [];
+      indexes = result['indexes'];
+
+      //Опять заново парсим
       let result2 = await page.evaluate(
         (indexes) => {
           //Теперь добавляем проценты только нужных нам акций (номер элемента берем из массива indexes)
@@ -114,22 +122,19 @@ setInterval(() => {
       );
 
       console.log(result2['secondValues'], getDate(), 'второй');
-      console.log(result2['indexes'].length, result['indexes'].length);
-      if (result2['indexes'].length === result['indexes'].length) {
-        let message = '';
-        for (let i = 0; i < result2['secondValues'].length; i++) {
-          if (Math.abs(result2['secondValues'][i] - result['firstValues'][i]) > condition) {
-            message += `${i}.${result2['tickerNames'][i]} = ${roundNumber(result2['secondValues'][i] - result['firstValues'][i])}% \n`;
-            console.log(roundNumber(result2['secondValues'][i] - result['firstValues'][i]));
-          }
+
+      //Создаем сообщение из названий акций, которые удовлетворяют условию
+      let message = '';
+      for (let i = 0; i < result2['secondValues'].length; i++) {
+        if (result2['secondValues'][i] - result['firstValues'][i] > condition) {
+          message += `${i}.${result2['tickerNames'][i]} = ${roundNumber(result2['secondValues'][i] - result['firstValues'][i])}% \n`;
+          console.log(result2['secondValues'][i], result['firstValues'][i]);
         }
-
-        console.log(message, getDate());
-
-        bot.sendMessage(25347317, message);
-      } else {
-        console.log('нихуя');
       }
+
+      console.log(message, getDate());
+
+      bot.sendMessage(25347317, message);
     } catch (error) {
       console.log(error.response);
       //=> 'Internal server error ...'
